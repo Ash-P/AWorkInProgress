dc6939b4129d23607193501d43b31df122f4d36d
tomblelloch <48826715+tomblelloch@users.noreply.github.com>
2019-04-11 09:54:08 +0100

    Update TestData.java

--- TestData.java
+++ TestData.java
@@ -5,15 +5,15 @@ public abstract class TestData implements storage{
         bookData bookData = new bookData();
         targetData targetData = new targetData();
 
-        userData.userID = 10321;// what is our id number convention
+        userData.userID = 1
         userData.username = "will_05";
         userData.totalPagesRead = 1039;
         userData.totalBooksRead = 2;
-        userData.booksCompletedOnADate = "";
+        userData.booksCompletedOnADate = "04-04-2019,2";
 
 
-        bookData.bookID = 00011;// what is our id number convention
-        bookData.status = 1;// need to check status number
+        bookData.bookID = 1
+        bookData.status = 0;
         bookData.title = "Harry Potter: The Philosopher's Stone";
         bookData.author = "J. K. Rowling";
         bookData.publisher = "Bloomsbury Publishing";
@@ -24,14 +24,14 @@ public abstract class TestData implements storage{
         bookData.dateStarted = "27 March 2019";
         bookData.dateCompleted = "04 April 2019";
         bookData.description = "It is a story about q boy whose life changed after he was accepted into magic school";
-        bookData.pagesRead = "223";//shouldn't this be ints
-        bookData.pagesReadOnADate = "24";
+        bookData.pagesRead = 223
+        bookData.pagesReadOnADate = "27-03-2019,100" "04-04-2019,123";
 
-        targetData.targetID = 10011;// what is our id number convention
-        targetData.targetType = 1;//need the target type codes
+        targetData.targetID = 1
+        targetData.targetType = 1
         targetData.isComplete = true;
         targetData.deadlineDate = "04 April 2019";
-        targetData.bookID = 00011;// what is our id number convention
+        targetData.bookID = 1;// what is our id number convention
         targetData.targetValue = 223;
         targetData.valueRemaining = 0;
        

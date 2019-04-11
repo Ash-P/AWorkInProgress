d440134a4cb6bafd3acebcde7b8574dc8a95694e
tomblelloch <48826715+tomblelloch@users.noreply.github.com>
2019-04-11 05:49:25 +0100

    Update alldata.java

--- alldata.java
+++ alldata.java
@@ -1,10 +1,8 @@
-
-
 import java.util.ArrayList;
 
 public class alldata {
-	public static ArrayList<storage.bookData> bookStore;
 	public static storage.userData userStore;
+	public static ArrayList<storage.bookData> bookStore;
 	public static ArrayList<storage.targetData> targetStore;	
 	
 }

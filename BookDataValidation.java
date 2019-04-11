a866aaa26d2fa6a70454d3028f5c95462a256a44
tomblelloch <48826715+tomblelloch@users.noreply.github.com>
2019-04-11 05:47:42 +0100

    Update BookDataValidation.java

--- BookDataValidation.java
+++ BookDataValidation.java
@@ -1,16 +1,16 @@
 
 public class BookDataValidation {
-
 	
 	protected static boolean validBookDataString(String string) {
 		if(string.contains("@") || string.contains(";")) {
 			return true;
 		}
 		return false;
-	
-		
 	}
 	
-	
+	protected static boolean validBookDataInteger(String string) {
+		if(string.matches("[0-9]+")) return false;
+		return true;
+	}
 	
 }

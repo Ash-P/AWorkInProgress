e362baafdbea2cb1ae621912dc15e85dacc9897a
tomblelloch <48826715+tomblelloch@users.noreply.github.com>
2019-04-12 18:26:55 +0100

    Update AchievementGUI.java

--- AchievementGUI.java
+++ AchievementGUI.java
@@ -1,125 +1,93 @@
-import javafx.application.Application;
 import javafx.geometry.Pos;
+import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
-import javafx.stage.Stage;
 
 
-public class AchievementGUI extends Application {
+public class AchievementGUI {
+	
+	public static final int[] PAGE_ACHIEVEMENTS = { 1, 10, 50, 100, 250, 500, 1000, 5000, 10000 };
+	public static final int[] BOOK_ACHIEVEMENTS = { 1, 5, 10, 25, 50, 100, 150, 250, 500 };
+	private static int totalPagesRead = alldata.userStore.totalPagesRead;
+	private static int totalBooksRead = alldata.userStore.totalPagesRead;
+	private static int pageAchievsUnlocked = alldata.userStore.pageAchievsUnlocked;
+	private static int bookAchievsUnlocked = alldata.userStore.bookAchievsUnlocked;
+	
+	private static Circle p1, p10, p50, p100, p250, p500, p1000, p5000, p10000;
+    private static Circle b1, b5, b10, b25, b50, b100, b150, b250, b500;
+    private static boolean isDone;
+    private static AnchorPane layout = new AnchorPane();
+    private static Group group;
 
-    private Circles p1;
-    private Circles p10;
-    private Circles p50;
-    private Circles p100;
-    private Circles p250;
-    private Circles p500;
-    private Circles p1000;
-    private Circles p5000;
-    private Circles p10000;
-    private Circles b1;
-    private Circles b5;
-    private Circles b10;
-    private Circles b25;
-    private Circles b50;
-    private Circles b100;
-    private Circles b150;
-    private Circles b250;
-    private Circles b500;
+    
+    //TODO check if has been awarded already when isDone=true
+    //TODO alerts - for newly completed achievements
+    
+    public AchievementGUI() {
+    	createUI();
+    	
+        Scene scene = new Scene(group,650,450);
+        MAIN.mainStage.setScene(scene);
+        MAIN.mainStage.setTitle("Achievements");
+    }
+    
+    private void createUI() {
+        p1 = createACircle(40, 121, getPageIsMet(1));
+        p10 = createACircle(125, 121, getPageIsMet(10));
+        p50 = createACircle(216, 121, getPageIsMet(50));
+        p100 = createACircle(40, 200, getPageIsMet(100));
+        p250 = createACircle(125, 200, getPageIsMet(250));
+        p500 = createACircle(216, 200, getPageIsMet(500));
+        p1000 = createACircle(40, 276, getPageIsMet(1000));
+        p5000 = createACircle(125, 276, getPageIsMet(5000));
+        p10000 = createACircle(216, 276, getPageIsMet(10000));
+        
+        b1 = createACircle(363, 121, getBookIsMet(1));
+        b5 = createACircle(454, 121, getBookIsMet(5));
+        b10 = createACircle(538, 121, getBookIsMet(10));
+        b25 = createACircle(363, 200, getBookIsMet(25));
+        b50 = createACircle(454, 200, getBookIsMet(50));
+        b100 = createACircle(538, 200, getBookIsMet(100));
+        b150 = createACircle(363, 276, getBookIsMet(150));
+        b250 = createACircle(454, 276, getBookIsMet(250));
+        b500 = createACircle(538, 276, getBookIsMet(500));
 
-    private boolean isDone;
-
-
-    public static void  main(String[] args){
-        launch(args);
+        loadLabels("Pages Read", 14, 32, 30);
+        loadLabels("Books Read", 337, 32, 30);
+        loadLabels("1",36,112,13);
+        loadLabels("10",117,112,13);
+        loadLabels("50",209,112,13);
+        loadLabels("100",29,191,13);
+        loadLabels("250", 114,191,13);
+        loadLabels("500",205,191,13);
+        loadLabels("1000",26,267,13);
+        loadLabels("5000",111,267,13);
+        loadLabels("10000",198,267,13);
+        loadLabels("1",359,112,13);
+        loadLabels("5",450,112,13);
+        loadLabels("10",531, 112,13);
+        loadLabels("25", 356,191,13);
+        loadLabels("50", 447,191,13);
+        loadLabels("100", 528, 191,13);
+        loadLabels("150",353,267,13);
+        loadLabels("250", 444,267,13);
+        loadLabels("500",528,267,13);     
+        loadLabels("Your pages read: " + totalPagesRead, 25, 329, 15);
+        loadLabels("Pages until next achievement: " + (PAGE_ACHIEVEMENTS[pageAchievsUnlocked+1] - totalPagesRead), 25, 360, 15);
+        loadLabels("Your books read: " + totalBooksRead, 337, 329, 15);
+        loadLabels("Books until next achievement: " + (BOOK_ACHIEVEMENTS[bookAchievsUnlocked+1] - totalBooksRead), 337, 360, 15);
+        
+        group = new Group(layout);
     }
 
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        AnchorPane layout = new AnchorPane();
-
-        primaryStage.setTitle("Achievements");
-
-        p1 = new Circles(layout,40,121);
-        p10 = new Circles(layout,125,121);
-        p50 = new Circles(layout,216,121);
-        b1 = new Circles(layout,363,121);
-        b5 = new Circles(layout,454,121);
-        b10 = new Circles(layout,538,121);
-        p100 = new Circles(layout,40,200);
-        p250 = new Circles(layout,125,200);
-        p500 = new Circles(layout,216,200);
-        b25 = new Circles(layout,363,200);
-        b50 = new Circles(layout,454,200);
-        b100 = new Circles(layout,538,200);
-        p1000 = new Circles(layout,40,276);
-        p5000 = new Circles(layout,125,276);
-        p10000 = new Circles(layout,216,276);
-        b150 = new Circles(layout,363,276);
-        b250 = new Circles(layout,454,276);
-        b500 = new Circles(layout,538,276);
-
-
-        checkTotalBooks();
-        checkTotalPages();
-
-
-        p1.createCircles();
-        p10.createCircles();
-        p50.createCircles();
-        b1.createCircles();
-        b5.createCircles();
-        b10.createCircles();
-        p100.createCircles();
-        p250.createCircles();
-        p500.createCircles();
-        b25.createCircles();
-        b50.createCircles();
-        b100.createCircles();
-        p1000.createCircles();
-        p5000.createCircles();
-        p10000.createCircles();
-        b150.createCircles();
-        b250.createCircles();
-        b500.createCircles();
-
-        loadLabels("1",36,112,13,layout);
-        loadLabels("10",117,112,13,layout);
-        loadLabels("50",209,112,13,layout);
-        loadLabels("100",29,191,13,layout);
-        loadLabels("250", 114,191,13,layout);
-        loadLabels("500",205,191,13,layout);
-        loadLabels("1000",26,267,13,layout);
-        loadLabels("5000",111,267,13,layout);
-        loadLabels("10000",198,267,13,layout);
-        loadLabels("1",359,112,13,layout);
-        loadLabels("5",450,112,13,layout);
-        loadLabels("10",531, 112,13,layout);
-        loadLabels("25", 356,191,13,layout);
-        loadLabels("50", 447,191,13,layout);
-        loadLabels("100", 528, 191,13,layout);
-        loadLabels("150",353,267,13,layout);
-        loadLabels("250", 444,267,13,layout);
-        loadLabels("500",528,267,13,layout);
-        loadLabels("Total Pages Read",14,32,32,layout);
-        loadLabels("Total Books Read",337,32,32,layout);
-        loadLabels("You have read: ",25,329,15,layout);
-        loadLabels("Pages until next achievement: ",25,360,15,layout);
-        loadLabels("You have read: ",337,329,15,layout);
-        loadLabels("Books until next achievement: ",337,360,15,layout);
-
-        Scene scene = new Scene(layout, 600,400);
-        primaryStage.setScene(scene);
-        primaryStage.show();
-
-
-    }
-
-    public void loadLabels(String text, int layX, int layY, int fSize, AnchorPane layout){
+    private static void loadLabels(String text, int layX, int layY, int fSize){
         Label label = new Label(text);
         label.setLayoutX(layX);
         label.setLayoutY(layY);
@@ -128,107 +96,70 @@ public class AchievementGUI extends Application {
 
         layout.getChildren().add(label);
     }
-
-    public void checkTotalPages(){
-        int pagesRead = alldata.userStore.totalPagesRead;
-
-        if(pagesRead >= 0){
-            p1.setDone(true);
-        }
-        else if(pagesRead >= 10){
-            p10.setDone(true);
-        }
-        else if(pagesRead >= 50){
-            p50.setDone(true);
-        }
-        else if(pagesRead >= 100){
-            p100.setDone(true);
-        }
-        else if(pagesRead >= 250){
-            p250.setDone(true);
-        }
-        else if(pagesRead >= 500){
-            p500.setDone(true);
-        }
-        else if(pagesRead >= 1000){
-            p1000.setDone(true);
-        }
-        else if(pagesRead >= 5000){
-            p5000.setDone(true);
-        }
-        else if(pagesRead >= 10000){
-            p10000.setDone(true);
-        }
-
+    
+    private static Boolean getPageIsMet(int value) {
+    	if(totalPagesRead >= value) return true; 
+    	return false;
+    }
+    
+    private static Boolean getBookIsMet(int value) {
+    	if(totalBooksRead >= value) return true; 
+    	return false;
     }
 
-    public void checkTotalBooks(){
-        int booksRead = alldata.userStore.totalBooksRead;
+    private static Circle createACircle(int layX, int layY, Boolean isMet){
+        Circle circle = new Circle();
+        circle.setRadius(26);
+        circle.setLayoutX(layX);
+        circle.setLayoutY(layY);
+        
+        if(isMet) circle.setFill(Color.GREEN);
+        else circle.setFill(Color.GREY);
 
-        if(booksRead >= 1){
-            b1.setDone(true);
-        }
-        else if(booksRead >= 5){
-            b5.setDone(true);
-        }
-        else if(booksRead >= 10){
-            b10.setDone(true);
-        }
-        else if(booksRead >= 25){
-            b25.setDone(true);
-        }
-        else if(booksRead >= 50){
-            b50.setDone(true);
-        }
-        else if(booksRead >= 100){
-            b100.setDone(true);
-        }
-        else if(booksRead >= 150){
-            b150.setDone(true);
-        }
-        else if(booksRead >= 250){
-            b250.setDone(true);
-        }
-        else if(booksRead >= 500){
-            b500.setDone(true);
-        }
-
+        layout.getChildren().add(circle);
+        return circle;
     }
-
-    private class Circles extends Circle {
-
-        private final AnchorPane layout;
-        private final int layX;
-        private final int layY;
-
-        private Circles(AnchorPane layout, int layX, int layY){
-            this.layout = layout;
-            this.layX = layX;
-            this.layY = layY;
-            isDone = false;
-        }
-
-        public void createCircles(){
-            Circle circle = new Circle();
-            circle.setRadius(26);
-            circle.setLayoutX(layX);
-            circle.setLayoutY(layY);
-
-            if(!isDone())
-                circle.setFill(Color.WHITE);
-            else
-                circle.setFill(Color.GREEN);
-
-            layout.getChildren().add(circle);
-
-        }
-
-        public boolean isDone() {
-            return isDone;
-        }
-
-        public void setDone(boolean done) {
-            isDone = done;
-        }
+    
+    public static void updateTargets() {
+    	int totalPagesRead = alldata.userStore.totalPagesRead;
+    	int totalBooksRead = alldata.userStore.totalPagesRead;
+    	int pageAchievsUnlocked = alldata.userStore.pageAchievsUnlocked;
+    	int bookAchievsUnlocked = alldata.userStore.bookAchievsUnlocked;
+    	
+    	while(true) {
+	    	if(totalPagesRead >= PAGE_ACHIEVEMENTS[pageAchievsUnlocked + 1]) {
+	    		pageAchievsUnlocked++;
+	    	} else break;
+    	}
+    	while(true) {
+	    	if(totalBooksRead >= BOOK_ACHIEVEMENTS[bookAchievsUnlocked + 1]) {
+	    		bookAchievsUnlocked++;
+	    	} else break;
+    	}
+    	if(pageAchievsUnlocked > alldata.userStore.pageAchievsUnlocked) {
+    		createAlert(true, pageAchievsUnlocked);
+    		alldata.userStore.pageAchievsUnlocked = pageAchievsUnlocked;
+    	}
+    	if(bookAchievsUnlocked > alldata.userStore.bookAchievsUnlocked) {
+    		createAlert(false, bookAchievsUnlocked);
+    		alldata.userStore.bookAchievsUnlocked = bookAchievsUnlocked;
+    	}
     }
+    
+    private static void createAlert(Boolean typeIsPages, int newIndex) {
+    	Alert dialog = new Alert(AlertType.INFORMATION);
+		dialog.setTitle("Achievement Progress");
+		if(typeIsPages) {
+			dialog.setHeaderText("New page achievement unlocked");
+			if(newIndex < 8) dialog.setContentText("Achievement earnt: " + PAGE_ACHIEVEMENTS[newIndex] + " pages read.\nNext level: " + PAGE_ACHIEVEMENTS[newIndex+1] + " pages.");
+			else dialog.setContentText("Achievement earnt: " + PAGE_ACHIEVEMENTS[newIndex] + " pages read.\\nAll page achievements now completed!");
+		}
+		else {
+			dialog.setHeaderText("New book achievement unlocked");
+			if(newIndex < 8) dialog.setContentText("Achievement earnt: " + BOOK_ACHIEVEMENTS[newIndex] + " books read.\nNext level: " + BOOK_ACHIEVEMENTS[newIndex+1] + " books.");
+			else dialog.setContentText("Achievement earnt: " + BOOK_ACHIEVEMENTS[newIndex] + " books read.\\nAll book achievements now completed!");
+		}
+		dialog.showAndWait(); //the dialog must be confirmed before continuing
+    }
+    
 }

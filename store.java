cdc01ca597efe920ad864add2b2424840c397270
tomblelloch <48826715+tomblelloch@users.noreply.github.com>
2019-04-13 14:15:42 +0100

    Update store.java

--- store.java
+++ store.java
@@ -155,13 +155,18 @@ public class store implements storage {
 			bookdata.genre = properties[10];
 			bookdata.description = properties[11];
 			
+			try {
+				if(!properties[9].trim().equals(""))
+					bookdata.publicationYear = Integer.parseInt(properties[9]);
+				else bookdata.publicationYear = -1;
+				if(!properties[12].trim().equals(""))
+					bookdata.pagesRead = Integer.parseInt(properties[12]);
+				else bookdata.pagesRead = 0;
+			} catch(NumberFormatException e) {
+				bookdata.publicationYear = -1;
+				bookdata.pagesRead = 0;
+			}
 			
-			if(!properties[9].trim().equals(""))
-				bookdata.publicationYear = Integer.parseInt(properties[9]);
-			else bookdata.publicationYear = -1;
-			if(!properties[12].trim().equals(""))
-				bookdata.pagesRead = Integer.parseInt(properties[12]);
-			else bookdata.pagesRead = 0;
 			try {
 				bookdata.pagesReadOnADate = properties[13];
 			} catch (Exception e) {
